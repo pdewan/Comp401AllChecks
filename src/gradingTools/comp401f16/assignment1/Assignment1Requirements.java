b05ffdbba20f78ccbf2ef048837c2fc110bd2c76
Andrew Ghobrial <awghobrial@gmail.com>
2016-09-13 17:57:35 -0400

    fix checks

--- src/gradingTools/comp401f16/assignment1/Assignment1Requirements.java
+++ src/gradingTools/comp401f16/assignment1/Assignment1Requirements.java
@@ -1,5 +1,6 @@
 package gradingTools.comp401f16.assignment1;
 
+import grader.basics.execution.GradingMode;
 import grader.junit.AJUnitProjectRequirements;
 import gradingTools.comp401f15.assignment1.testcases.ImageEnclosedTestCase;
 import gradingTools.comp401f16.assignment1.testcases.Assignment1Suite;
@@ -11,14 +12,16 @@ public class Assignment1Requirements extends AJUnitProjectRequirements {
 	public static final String MAIN_SUBSTRING = "main.Assignment";
 	public static final String ITERATOR_SUSTRING = "ScanningIterator";
     public Assignment1Requirements() {
-    	addDueDate("08/31/2016 23:59:00", 1.05); // wed before
-    	addDueDate("09/02/2016 23:59:00", 1); // fri due
-    	addDueDate("09/05/2016 23:55:00", 0.95);// mon after due
-    	addDueDate("09/09/2016 23:55:00", 0.85); // fri after due
-    	addDueDate("12/04/2016 23:55:00", 0.8); // infinity
-
+    	addDueDate("09/01/2016 00:59:00", 1.05); // wed before
+    	addDueDate("09/03/2016 00:59:00", 1); // fri due
+    	addDueDate("09/06/2016 00:59:00", 0.95);// mon after due
+    	addDueDate("09/10/2016 00:59:00", 0.85); // fri after due
+    	addDueDate("12/05/2016 00:59:00", 0.8); // infinity
+    	
         // Functionality
     	addJUnitTestSuite(Assignment1Suite.class);
+    	GradingMode.setGraderRun(true);
+    	
     	// Method calls
     	addFeature("3 Required Methods Called in Main or Iterator (max 15pts)", 18, 
 //    	   		new CheckstyleMethodDefinedTestCase("Parser", 	"@parseNumber:\\*->\\*"),
@@ -27,23 +30,23 @@ public class Assignment1Requirements extends AJUnitProjectRequirements {
     	   				"processInput:->void"),
     	   		new CheckstyleMethodCalledTestCase(
     	    	   		"main.Assignment",    	    	   		
-    	    	   		"indexOf:String;char"),
+    	    	   		"indexOf:String;char;int->int"),
     	    	 new CheckstyleMethodCalledTestCase(
     	    	    	"main.Assignment",  		
     	    	   		"scanString:String->void"    	    	   		
     	    	   		),
     	    	 new CheckstyleMethodCalledTestCase(
     	    	    	"ScanningIterator",    	    	   		
-    	    	    	 "indexOf:String;char")); 
+    	    	    	 "indexOf:String;char;int->int")); 
     	    	
     	addFeature("indexOfNot Called in Main or Iterator (max 3pts)", 6, true,
     	   		new CheckstyleMethodCalledTestCase(
     	    	   		"main.Assignment",    	    	   		
-    	    	   		"indexOfNot:String;char"),
+    	    	   		"indexOfNot:String;char;int->int"),
     	    	 
     			new CheckstyleMethodCalledTestCase(
     	   				"ScanningIterator",    	    	   		
-    	    	   		"indexOfNot:String;char")
+    	    	   		"indexOfNot:String;char;int->int")
     	    	 );
     	    	 
     	addFeature("Iterator Methods Called in Main", 10, true,
