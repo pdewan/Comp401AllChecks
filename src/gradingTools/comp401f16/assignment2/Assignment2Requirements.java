c932e2b101132c29b293520e8cc5c1d27b8fd6d4
Semion Piskarev <semionpiskarev@gmail.com>
2016-09-15 10:28:19 -0400

    Semion's first draft of Assignment2 AllChecks file

---
+++ src/gradingTools/comp401f16/assignment2/Assignment2Requirements.java
@@ -0,0 +1,33 @@
+package gradingTools.comp401f16.assignment2;
+
+import grader.basics.execution.GradingMode;
+import grader.junit.AJUnitProjectRequirements;
+import gradingTools.comp401f15.assignment1.testcases.ImageEnclosedTestCase;
+import gradingTools.comp401f16.assignment2.testcases.Assignment2Suite;
+import gradingTools.sharedTestCase.ClassDefinedTestCase;
+import gradingTools.sharedTestCase.IllegalImportOrCallTestCase;
+
+
+public class Assignment2Requirements extends AJUnitProjectRequirements {
+
+    public Assignment2Requirements() {
+     	addDueDate("09/08/2016 00:59:00", 1.05); // wed before
+     	addDueDate("09/10/2016 00:59:00", 1); // fri due
+     	addDueDate("09/13/2016 00:59:00", 0.95);// mon after due
+     	addDueDate("09/17/2016 00:59:00", 0.85); // fri after due
+     	addDueDate("12/05/2016 00:59:00", 0.8); // infinity
+     	
+     	
+      // Functionality
+    	addJUnitTestSuite(Assignment2Suite.class);
+    	GradingMode.setGraderRun(true);
+    	
+    	
+        addFeature("Screenshots enclosed", 10, new ImageEnclosedTestCase());
+        addManualFeature("Breakpoint step into/over/return screenhots", 20);
+        
+        addRestriction("Illegal import or call", 25, new IllegalImportOrCallTestCase());
+        addRestriction("Scanner Bean Tagged ", 5, new ClassDefinedTestCase("@ScannerBean"));
+
+    }
+}
