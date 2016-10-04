d25bb9556dd022ac6cc1d377f3e0183db3f2fd75
pdewan <dewan@cs.unc.edu>
2016-10-04 11:33:36 -0400

    Ran A4 tests

--- Test Data/Comp401F16/Assignment4/Test, Student(tstudent)/Submission attachment(s)/Assignment 3/Assignment 3/src/main/TokenScanner.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-import java.util.Iterator;
-
-public interface TokenScanner extends Iterator<Token>{
-	public void setScannedString(String scannedString);
-	public String getScannedString();
-}
