d00ceae5a8f88c754119fdf43a2996d21a2e0f0e
avitkus <avitkus7@gmail.com>
2017-08-31 18:11:44 -0400

    Added A2-12 requirements for F17. All refer to checks from F16 but have updated deadlines

---
+++ src/gradingTools/comp401f17/assignment10/Assignment10Requirements.java
@@ -0,0 +1,125 @@
+package gradingTools.comp401f17.assignment10;
+
+import grader.basics.execution.GradingMode;
+import grader.junit.AJUnitProjectRequirements;
+import gradingTools.comp401f16.assignment1.testcases.ImageEnclosedTestCase;
+import gradingTools.comp401f17.assignment10.testcases.Assignment10Suite;
+import gradingTools.sharedTestCase.NoWarningOrErrorTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckStyleClassDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckStyleEditablePropertyDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckStyleIllegalImportOrCallTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckStyleMagicNumberTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckStyleMinCalledMethodsTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckStylePropertyDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckStyleVariableHasClassTypeTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleClassInstantiatedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleConstructorDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleInterfaceDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleMethodCalledTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleMethodDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstylePatternBasedSuperTypeDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleSuperTypeDefinedTestCase;
+import gradingTools.sharedTestCase.checkstyle.ClassHasAtLeastOneInterfaceTestCase;
+
+public class Assignment10Requirements  extends AJUnitProjectRequirements{
+	public Assignment10Requirements(){
+		addDueDate("11/09/2017 00:59:00", 1.05); 	// wed before
+     	addDueDate("11/11/2017 00:59:00", 1); 		// fri due
+     	
+     	// Functionality
+     	GradingMode.setGraderRun(true);
+     	
+     	addJUnitTestSuite(Assignment10Suite.class);
+     	
+     	addFeature("Public methods are in implemented interfaces", 10, 
+     			new ClassHasAtLeastOneInterfaceTestCase());
+     	
+     	addRestriction("Variables should have interface, not class, type", 10, 
+     			new CheckStyleVariableHasClassTypeTestCase());
+     	
+     	addRestriction("No magic numbers", 10, new CheckStyleMagicNumberTestCase());
+     	
+     	// Preconditions
+     	addFeature("Preconditions defined", 8, 
+     			new CheckstyleMethodDefinedTestCase("BridgeScene", "@prePassed:->boolean"),
+     			new CheckstyleMethodDefinedTestCase("BridgeScene", "@preFailed:->boolean"),
+     			new CheckstyleMethodDefinedTestCase("BridgeScene", "@preSay:->boolean"),
+     			new CheckstyleMethodDefinedTestCase("BridgeScene", "@preApproach:->boolean")
+     			);
+     	
+     	
+     	addFeature("Event notifications fired and received", 6,
+     			new CheckstyleInterfaceDefinedTestCase("BridgeScene", "PropertyListenerRegisterer"),
+     			new CheckstyleClassInstantiatedTestCase("BridgeScene" ,"PropertyChangeEvent"),
+     			new CheckstyleMethodCalledTestCase("ConsoleSceneView", "(.*)!addPropertyChangeListener:PropertyChangeListener->void")
+     			);
+     	
+     	// command objects
+     	addFeature("Command objects created for parsing", 8,
+     			new CheckstyleInterfaceDefinedTestCase("SayCommand", "Runnable"),
+     			new CheckstyleConstructorDefinedTestCase("SayCommand", ":@BridgeScene; String"),
+     			new CheckstyleInterfaceDefinedTestCase("MoveCommand", "Runnable"),
+     			new CheckstyleConstructorDefinedTestCase("MoveCommand", ":@Avatar; int; int")
+     			);
+     			
+     	// parsing methods, run() calls
+     	addFeature("Parsing methods made, setter calls run()", 10,
+     			new CheckstyleMethodDefinedTestCase("CommandInterpreter", "@parseMove:*->Runnable"),
+     			new CheckstyleMethodDefinedTestCase("CommandInterpreter", "@parseSay:*->Runnable"),
+     			new CheckstyleMethodCalledTestCase("CommandInterpreter", "@parseMove:*->Runnable"),
+     			new CheckstyleMethodCalledTestCase("CommandInterpreter", "@parseSay:*->Runnable"),
+     			new CheckstyleMethodCalledTestCase("CommandInterpreter", "Runnable!run:-> void")
+     			);
+     	
+     	// animator, animation commands
+     	addFeature("Animator and animation commands created and used", 6,
+     			new CheckstyleMethodCalledTestCase("Animator", "(.*)!sleep:long->void"),
+     			new CheckstyleMethodCalledTestCase("AnimatingCommand", "@Animator!@animateAvatar:@Avatar->void"),
+     			new CheckstyleConstructorDefinedTestCase("AnimatingCommand",":@Animator;@Avatar")
+     			);
+     	
+     	// asynch methods, thread instantiations
+     	addFeature("Asynchronous methods create new threads", 7,
+     			new CheckstyleClassInstantiatedTestCase("CommandInterpreter", "AnimatingCommand"),
+     			new CheckstyleMethodCalledTestCase("CommandInterpreter", "Thread!start:->void"),
+     			new CheckstyleMethodDefinedTestCase("CommandInterpreter", "@asynchronousArthur:->void"),
+     			new CheckstyleMethodDefinedTestCase("CommandInterpreter", "@asynchronousGalahad:->void"),
+     			new CheckstyleMethodDefinedTestCase("CommandInterpreter", "@asynchronousLancelot:->void"),
+     			new CheckstyleMethodDefinedTestCase("CommandInterpreter", "@asynchronousRobin:->void"),
+     			new CheckstyleMethodCalledTestCase("main.Assignment10", "(.*)!@asynchronous(.*):->void")
+     			);
+     	
+     	// EC: asynch guard
+     	addFeature("Asynchronous guard", 2, true,
+     			new CheckstyleMethodDefinedTestCase("CommandInterpreter", "@asynchronousGuard:->void")
+     			);
+     	
+     	// EC: setEnabled calls, button properties
+     	addFeature("CommandInterpreter new buttons respond to preconditions", 6 ,true,
+     			new CheckstyleMethodCalledTestCase("CommandInterpreterController", "(.*)!addPropertyChangeListener:PropertyChangeListener->void"),
+     			new CheckstyleMethodCalledTestCase("CommandInterpreterController", "JButton!setEnabled:boolean->void"),
+     			new CheckStylePropertyDefinedTestCase("CommandInterpreterController", "Say", "JButton"),
+     			new CheckStylePropertyDefinedTestCase("CommandInterpreterController", "Approach", "JButton"),
+     			new CheckStylePropertyDefinedTestCase("CommandInterpreterController", "Pass", "JButton"),
+     			new CheckStylePropertyDefinedTestCase("CommandInterpreterController", "Fail", "JButton")
+     			);
+     	
+     	// Manual: All preconditions shown, precondition events fired
+   	addManualFeature("Console shows all precondition events being fired", 10, false); 
+     	
+     	// Manual: asynch methods
+   	addManualFeature("Animation shows animations acting asynchronously", 10, false); 
+      
+     	// Manual, EC: buttons deactivated properly, work
+   	addManualFeature("CommandInterpreter buttons activate and deactivate properly", 9, true); 
+   	
+     	// Manual, EC: clap animation
+   	addManualFeature("Animation shows clapping guard", 3, true); 
+      
+     	addManualRestriction(INTERACTIVE_RUN, 5, 
+     			new NoWarningOrErrorTestCase("No OE Warnings", ".*(efresh|not in range).*", null, 0.3));
+     	
+     	addRestriction("Illegal import or call", 25, new CheckStyleIllegalImportOrCallTestCase());
+      
+	}//end method
+}
---
+++ src/gradingTools/comp401f17/assignment2/Assignment2Requirements.java
@@ -0,0 +1,53 @@
+package gradingTools.comp401f17.assignment2;
+
+import grader.basics.execution.BasicProjectExecution;
+import grader.basics.execution.GradingMode;
+import grader.junit.AJUnitProjectRequirements;
+import gradingTools.comp401f15.assignment1.testcases.ImageEnclosedTestCase;
+import gradingTools.comp401f17.assignment2.testcases.Assignment2Suite;
+import gradingTools.sharedTestCase.checkstyle.CheckStyleIllegalImportOrCallTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleClassInstantiatedTestCase;
+import gradingTools.sharedTestCase.checkstyle.CheckstyleMethodCalledTestCase;
+
+public class Assignment2Requirements extends AJUnitProjectRequirements {
+
+    public Assignment2Requirements() {
+     	addDueDate("09/07/2017 00:59:00", 1.05); // wed before
+     	addDueDate("09/09/2017 00:59:00", 1); // fri due
+     	addDueDate("09/13/2017 00:59:00", 0.90);// mon after due
+     	addDueDate("09/16/2017 00:59:00", 0.75); // fri after due
+     	
+     	BasicProjectExecution.setReRunInfiniteProcesses(true);
+      // Functionality
+    	addJUnitTestSuite(Assignment2Suite.class);
+    	GradingMode.setGraderRun(true);
+    	
+    	// Method calls
+    	addFeature("ScanString and IndexOf now called in ScannerBean", 10,
+	   		new CheckstyleMethodCalledTestCase(
+	   				"ScannerBean", 
+	   				"scanString:String->void"),
+	   		new CheckstyleMethodCalledTestCase(
+	    	   		"ScannerBean",    	    	   		
+	    	   		"indexOf:String;char;int->int")); 
+    	// Method calls
+    	addFeature("IndexOf called in ScanningIterator", 5,
+    			true,
+    			new CheckstyleMethodCalledTestCase(
+    	    	    	"ScanningIterator",    	    	   		
+    	    	    	 "indexOf:String;char;int->int"));
+	   		
+    	// Classes Instantiated
+    	addFeature("Scanner Bean Instantiated in Corectly Named Main", 5,
+    	   		new CheckstyleClassInstantiatedTestCase(
+    	   				"main.Assignment2", 
+    	   				"ScannerBean")); 
+    	
+        addFeature("Screenshots enclosed", 10, new ImageEnclosedTestCase());
+        addManualFeature("Breakpoint step into/over/return screenhots", 20);
+
+     //   addFeature("Scanner Bean Tagged Correctly ", 5, new ClassDefinedTestCase("@ScannerBean"));
+        addRestriction("Illegal import or call", 25, new CheckStyleIllegalImportOrCallTestCase());
+
+    }
+}
