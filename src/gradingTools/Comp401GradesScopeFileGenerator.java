ff60b596d7eb4fb0478dce5d381dd03e51b8d86e
pdewan <dewan@cs.unc.edu>
2017-10-19 20:52:40 -0400

    gradescope generator

--- src/gradingTools/Comp401GradesScopeFileGenerator.java
+++ src/gradingTools/Comp401GradesScopeFileGenerator.java
@@ -1,9 +1,12 @@
 package gradingTools;
 
 public class Comp401GradesScopeFileGenerator {
+//	static String[] onyensToEmail = new String[] {
+//		"whglaser:whglaser@ad.unc.edu",
+//		"ellenecs:ellenecs@email.unc.edu"
+//	};
 	static String[] onyensToEmail = new String[] {
-		"hollowaj:jonah_holloway@med.unc.edu",
-		"ellenecs:ellenecs@email.unc.edu"
+		"whglaser:whglaser@ad.unc.edu"
 	};
 	public static void main (String[] args) {
 		GradeScopeFileGenerator.createAndWriteGeneratedFile("grades.csv", 
