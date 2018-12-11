3dd7db3fb767cf601e112863119506a4531ec9f6
pdewan <dewan@cs.unc.edu>
2018-12-11 11:38:22 -0500

    Gradescope assignment to sakai

--- src/gradingTools/Comp401GradescopeToGradeBookFileGenerator.java
+++ src/gradingTools/Comp401GradescopeToGradeBookFileGenerator.java
@@ -16,14 +16,22 @@ public class Comp401GradescopeToGradeBookFileGenerator {
 //	public static final String SAKAI_FILE_NAME = "Final_scores_F17_Sakai.csv";
 //	public static final String GRADE_COLUMN_NAME = "Final";
 	public static final String GRADESCOPE_FILE_NAME = 
-			Comp401GradeBookToGradesScopeFileGenerator.GRADES_DIR +
 			"Midterm_scores.csv";
+	public static final String GRADESCOPE_FULL_FILE_NAME = 
+			Comp401GradeBookToGradesScopeFileGenerator.GRADES_DIR +
+			GRADESCOPE_FILE_NAME;
+
 	public static final String SAKAI_FILE_NAME = 
 			Comp401GradeBookToGradesScopeFileGenerator.GRADES_DIR +
 			"Midterm_scores_F18_Sakai.csv";
 	public static final String GRADE_COLUMN_NAME = "Midterm";
 
-
+    public static final String gradeScopeFileName() {
+    	 return Comp401GradeBookToGradesScopeFileGenerator.GRADES_DIR + GRADE_COLUMN_NAME +"_scores.csv";
+    }
+    public static final String sakaiFileName() {
+   	 return Comp401GradeBookToGradesScopeFileGenerator.GRADES_DIR + GRADE_COLUMN_NAME + ".csv";
+   }
 	
 	static String[] onyensToEmail = new String[] {
 //		"whglaser:whglaser@ad.unc.edu"
@@ -31,14 +39,23 @@ public class Comp401GradescopeToGradeBookFileGenerator {
 
 	public static void main (String[] args) {
 //		Map aMap = GradebookConverter.gradeBookToMap(new File("D:\\UNCGoogleDrive\\401-f15\\grades\\gradebook_export-F17 -Template.csv"));
+//		GradebookGradescopeConverter.gradescopeToGradebook(
+//				GRADESCOPE_FILE_NAME,
+//				SAKAI_FILE_NAME,
+////				"D:\\UNCGoogleDrive\\401-f15\\grades\\Final_scores.csv", 
+////				"D:\\UNCGoogleDrive\\401-f15\\grades\\Final_scores_F17_Sakai.csv",
+//				Comp401GradeBookToGradesScopeFileGenerator.GRADEBOOK_TEMPLATE_FILE_NAME,
+//				onyensToEmail,
+//				GRADE_COLUMN_NAME);
 		GradebookGradescopeConverter.gradescopeToGradebook(
-				GRADESCOPE_FILE_NAME,
-				SAKAI_FILE_NAME,
+				gradeScopeFileName(),
+				sakaiFileName(),
 //				"D:\\UNCGoogleDrive\\401-f15\\grades\\Final_scores.csv", 
 //				"D:\\UNCGoogleDrive\\401-f15\\grades\\Final_scores_F17_Sakai.csv",
 				Comp401GradeBookToGradesScopeFileGenerator.GRADEBOOK_TEMPLATE_FILE_NAME,
 				onyensToEmail,
-				GRADE_COLUMN_NAME);
+				GRADE_COLUMN_NAME,
+				true);
 		
 	 
 	}
