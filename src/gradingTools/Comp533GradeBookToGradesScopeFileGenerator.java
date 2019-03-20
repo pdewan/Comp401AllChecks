ed878b12b6dd871dbc532e0cf981b5b44567fb61
pdewan <dewan@cs.unc.edu>
2019-03-19 20:48:50 -0400

    Gradescope file

--- src/gradingTools/Comp533GradeBookToGradesScopeFileGenerator.java
+++ src/gradingTools/Comp533GradeBookToGradesScopeFileGenerator.java
@@ -8,13 +8,20 @@ public class Comp533GradeBookToGradesScopeFileGenerator {
 	};
 //	public static final String GRADEBOOK_TEMPLATE_FILE_NAME = 
 //			"D:\\UNCGoogleDrive\\533\\grades\\gradebook_export-template.csv";
+//	public static final String GRADEBOOK_TEMPLATE_FILE_NAME = 
+//			"G:\\My Drive\\533\\grades\\gradebook_export-template.csv";
+	public static final String GRADES_DIR =  "G:\\My Drive\\533\\grades\\s19\\";
 	public static final String GRADEBOOK_TEMPLATE_FILE_NAME = 
-			"G:\\My Drive\\533\\grades\\gradebook_export-template.csv";
+//			"G:\\My Drive\\401-f15\\grades\\f18\\"
+			GRADES_DIR
+			+ "gradebook_export-template.csv";
 	
 	public static void main (String[] args) {
 		GradebookGradescopeConverter.gradebookToGradescope(
 				GRADEBOOK_TEMPLATE_FILE_NAME, 
-				"D:\\UNCGoogleDrive\\533\\grades\\gradescope_import-S18.csv",				
+				GRADES_DIR +
+				"gradescope_import-S19.csv",
+//				"D:\\UNCGoogleDrive\\533\\grades\\gradescope_import-S18.csv",				
 				onyensToEmail);
 		
 	 
